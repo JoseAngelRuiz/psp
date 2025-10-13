12a5ae82192f46a786ec599cf5afd5c4f755e916
José Ángel Ruiz Garcerán <joseruizgarceran@gmail.com>
2025-10-13 18:22:46 +0200

    Update lanzador.java

--- proceso/src/main/java/es/cifpcarlos3/lanzador.java
+++ proceso/src/main/java/es/cifpcarlos3/lanzador.java
@@ -5,13 +5,12 @@ import java.util.ArrayList;
 
 public class lanzador {
     public static void main(String[] args) {
-        Process nuevoProceso;
+        Process proceso;
         for (int i = 0; i < 10; i++) {
             try {
-                nuevoProceso = Runtime.getRuntime().exec("java -jar "
+                proceso = Runtime.getRuntime().exec("java -jar "
                         + "proceso-1.0-SNAPSHOT.jar " + "proceso" + i + " 10 " + "numeros.txt");
-                System.out.println("java -jar "
-                        + "proceso-1.0-SNAPSHOT.jar " + "proceso" + i + " 5 " + "numeros.txt");
+                System.out.println("Ejecutando");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
